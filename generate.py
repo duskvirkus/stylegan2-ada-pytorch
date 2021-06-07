8e34c23ad69a433e1191ba989347458b4ff56417
duskvirkus <duskvirkus@protonmail.com>
2021-06-07 16:37:07 -0600

    maybe?

--- generate.py
+++ generate.py
@@ -466,7 +466,7 @@ def generate_images(
                 z = torch.from_numpy(vec).to(device)
                 w = G.mapping(z, label, truncation_psi=truncation_psi, truncation_cutoff=8)
                 if save_vectors:
-                    np.save(f'{outdir}/vectors/seed{seed:04d}-w', w)
+                    np.save(f'{outdir}/vectors/seed{seed:04d}-w', w.cpu().detach().numpy())
                 img = G.synthesis(w, noise_mode=noise_mode, force_fp32=True)
             img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
             PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
